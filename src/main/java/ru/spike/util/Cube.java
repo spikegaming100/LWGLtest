fd2c82ffab3e163e7a2802edd639e9ce559125cf
spikegaming100 <114250271+spikegaming100@users.noreply.github.com>
2024-02-13 16:26:49 +0300

    Add files via upload

---
+++ src/main/java/ru/spike/util/Cube.java
@@ -0,0 +1,5 @@
+package ru.spike.util;
+
+public class Cube {
+
+}
---
+++ src/main/java/ru/spike/util/Launcher.java
@@ -0,0 +1,18 @@
+package ru.spike.util;
+
+import org.lwjgl.Version;
+
+public class Launcher {
+    public static void main(String[] args){
+        System.out.println(Version.getVersion());
+        WindowManager window = new WindowManager("xui", 640,480, false);
+        window.init();
+        while(!window.windowShouldClose()) {
+
+            window.update();
+
+        }
+        window.cleanup();
+
+    }
+}
