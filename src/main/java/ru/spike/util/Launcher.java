5ac8914e7929898307dbe7fc2c3a07305f61fb5e
spikegaming100 <114250271+spikegaming100@users.noreply.github.com>
2024-02-14 20:21:25 +0300

    rework

--- src/main/java/ru/spike/util/Launcher.java
+++ src/main/java/ru/spike/util/Launcher.java
@@ -5,7 +5,7 @@ import org.lwjgl.Version;
 public class Launcher {
     public static void main(String[] args){
         System.out.println(Version.getVersion());
-        WindowManager window = new WindowManager("xui", 640,480, false);
+        WindowManager window = new WindowManager("Main", 640,480, false);
         window.init();
         while(!window.windowShouldClose()) {
 
