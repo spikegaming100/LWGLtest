61d9248cc90a95b84831a32b86c939baff84c0cc
spikegaming100 <nikita.terekhin@gmail.com>
2024-02-23 03:03:22 +0300

    Da pohyi mne, the most fattest jar with dependencies

--- src/main/java/ru/spike/util/WindowManager.java
+++ src/main/java/ru/spike/util/WindowManager.java
@@ -33,6 +33,7 @@ public class WindowManager {
         GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 2);
         GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
         GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
+        GLFW.glfwWindowHint(GLFW.GLFW_STEREO, GL11.GL_FALSE);
 
         boolean maximized = false;
         if(width == 0 || height == 0) {
